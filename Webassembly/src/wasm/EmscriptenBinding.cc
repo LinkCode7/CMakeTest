abb523ebe09e2fe429bc94082e5e33a265c4e9e1
LinkCode7 <siyujianglin@163.com>
2023-08-30 16:56:52 +0800

    add project Webassembly

--- Webassembly/src/wasm/EmscriptenBinding.cc
+++ Webassembly/src/wasm/EmscriptenBinding.cc
@@ -3,18 +3,32 @@
 #include <emscripten/bind.h>
 #include <emscripten/val.h>
 
-#include <iostream>
 #include <sstream>
 
 namespace sindy
 {
-MyPoint::MyPoint(double xx, double yy, std::string const& str) : _x(xx), _y(yy), _str(str)
-{ // Js.Module.MyPoint()会执行构造
-    std::cout << ">>> MyPoint::MyPoint(double xx, double yy, const std::string& str)" << std::endl;
+Point::Point(double xx, double yy) : _x(xx), _y(yy)
+{ // Js.Module.Point()会执行构造
+    std::cout << ">>> Point::Point(double xx, double yy)" << std::endl;
 }
-MyPoint::~MyPoint()
+Point::~Point()
 { // Js.EmbindObject.delete()会执行析构
-    std::cout << ">>> MyPoint::~MyPoint()" << std::endl;
+    std::cout << ">>> Point::~Point()" << std::endl;
+}
+
+Point getPoint()
+{
+    return Point(100.1, 200.2);
+}
+
+StructObject getStructObject()
+{
+    // StructObject object(10, 10.101, "1010");
+    StructObject object;
+    object.num      = 10;
+    object.distance = 10.101;
+    object.name     = "1010";
+    return object;
 }
 
 emscripten::val getBuffer()
@@ -27,91 +41,30 @@ emscripten::val getBuffer()
     return emscripten::val(emscripten::typed_memory_view(bufferLength, byteBuffer));
 }
 
-TriangleData* getTriangleData(int params)
-{
-    TriangleData* pData = new TriangleData();
-    return pData;
-}
-
-emscripten::val getTriangleVertexs(const TriangleData& data)
-{
-    return emscripten::val::array(data.vertexs.begin(), data.vertexs.end());
-}
-
-emscripten::val getTriangleStr(const TriangleData& data)
-{
-    return emscripten::val::array(data.str.begin(), data.str.end());
-}
-emscripten::val getTrianglePoint(const TriangleData& data)
-{
-    return emscripten::val::array(data.points.begin(), data.points.end());
-}
-
 } // namespace sindy
 
-std::vector<EmbindObject> g_objects;
-
-void printFirstObject()
-{
-    auto const& arr  = EmbindManager::instance().data();
-    auto        size = arr.size();
-    std::cout << ">>>EmbindManager::instance().data().size() = " << size << std::endl;
-
-    if (size > 0)
-        std::cout << ">>>arr[0]->doubleValue() = " << arr[0]->doubleValue() << std::endl;
-}
-std::vector<EmbindObject*> getTestObject()
-{
-    auto object = new EmbindObject(100, 3.14, "obj");
-    EmbindManager::instance().add(object);
-
-    auto const& arr = EmbindManager::instance().data();
-
-    g_objects.clear();
-    for (auto const& pObject : arr)
-        g_objects.emplace_back(*pObject);
-
-    return arr;
-    // return emscripten::val::array(g_objects.begin(), g_objects.end());
-}
-
-EMSCRIPTEN_BINDINGS(sindy)
+EMSCRIPTEN_BINDINGS(EmscriptenBinding)
 {
     using namespace sindy;
 
-    emscripten::function("_getTestObject", &getTestObject);
-    emscripten::function("_printFirstObject", &printFirstObject);
-
-    // binding function
-    emscripten::value_object<Point>("Point").field("x", &Point::x).field("y", &Point::y);
-
     emscripten::function("_getPoint", &getPoint);
+    emscripten::function("_getStructObject", &getStructObject);
+
     emscripten::function("_getBuffer", &getBuffer, emscripten::allow_raw_pointers());
-    emscripten::function("_getTriangleData", &getTriangleData, emscripten::allow_raw_pointers());
 
-    // binding class
-    emscripten::class_<MyPoint>("MyPoint")
-        .constructor<double, double, std::string const&>()
-        .function("resetX", &MyPoint::resetX)
-        .function("getX", &MyPoint::getX)
-        .function("setX", &MyPoint::setX)
-        .function("getY", &MyPoint::getY)
-        .function("setY", &MyPoint::setY)
-        .class_function("getStringFromInstance", &MyPoint::getStringFromInstance);
+    // binding struct
+    emscripten::value_object<StructObject>("StructObject")
+        .field("num", &StructObject::num)
+        .field("distance", &StructObject::distance)
+        .field("name", &StructObject::name);
 
-    // 这种方式不仅把数据导出了，还不侵入TriangleData类的设计!
-    emscripten::class_<sindy::TriangleData>("TriangleData")
-        .function("_vertexs", &getTriangleVertexs, emscripten::allow_raw_pointers())
-        .function("_str", &getTriangleStr, emscripten::allow_raw_pointers())
-        .function("_points", &getTrianglePoint, emscripten::allow_raw_pointers());
-
-    emscripten::class_<EmbindObject>("EmbindObject")
-        .constructor<int, double, std::string const&>()
-        .function("intValue", &EmbindObject::intValue)
-        .function("doubleValue", &EmbindObject::doubleValue)
-        .function("stringValue", &EmbindObject::stringValue)
-        .function("setDouble", &EmbindObject::setDouble)
-        .class_function("doIt", &EmbindObject::doIt);
-
-    emscripten::register_vector<EmbindObject*>("vector_EmbindObject");
+    // binding class:overload function
+    emscripten::class_<Point>("Point")
+        .constructor<double, double>()
+        .function("multiply", &Point::multiply)
+        .property("x", emscripten::select_overload<double() const>(&Point::x),
+                  emscripten::select_overload<void(double)>(&Point::x))
+        .property("y", emscripten::select_overload<double() const>(&Point::y),
+                  emscripten::select_overload<void(double)>(&Point::y))
+        .class_function("getStringFromInstance", &Point::getStringFromInstance);
 }
--- Webassembly/src/wasm/EmscriptenBinding.h
+++ Webassembly/src/wasm/EmscriptenBinding.h
@@ -7,97 +7,38 @@
 
 namespace sindy
 {
-struct Point
-{
-    int x;
-    int y;
-};
-
-Point getPoint()
-{
-    Point point = {100, 200};
-    return point;
-}
-
-class MyPoint
+class Point
 {
     double _x;
     double _y;
 
-    std::string _str;
-
 public:
-    MyPoint(double xx, double yy, std::string const& str);
-    ~MyPoint();
+    Point(double xx, double yy);
+    ~Point();
 
-    inline int  x() const { return _x; }
-    inline void x(double value) { _x = value; }
+    inline double x() const { return _x; }
+    inline void   x(double value) { _x = value; }
 
     inline double y() const { return _y; }
     inline void   y(double value) { _y = value; }
 
-    // 内联调用
-    inline int    getX() const { return x(); }
-    inline void   setX(double value) { x(value); }
-    inline double getY() const { return y(); }
-    inline void   setY(double value) { y(value); }
+    void multiply(double value)
+    {
+        _x *= value;
+        _y *= value;
+    }
 
-    void resetX(double value) { _x = value; }
-
-    static std::string getStringFromInstance(const MyPoint& object) { return object._str; }
+    static double getStringFromInstance(const Point& object) { return object._x * object._y; }
 };
 
-class TriangleData
+struct StructObject
 {
-public:
-    std::vector<float> vertexs = {1.1, 2.2, 3.3};
-
-    std::vector<std::string> str    = {"msg", "msg"};
-    std::vector<MyPoint>     points = {{1, 1, "1"}, {2, 2, "2"}};
-    // std::vector<std::shared_ptr<MyPoint>> points = {std::make_shared<MyPoint>(1, "1"), std::make_shared<MyPoint>(2, "2")};
-
-    void test() { vertexs.clear(); }
+    int         num;
+    double      distance;
+    std::string name;
+    // StructObject(int num1, double distance1, std::string const& name1) : distance(num1), num(distance1), name(name1) {}
 };
 
 } // namespace sindy
 
-class EmbindObject
-{
-    int         _int;
-    double      _double;
-    std::string _string;
-
-    sindy::MyPoint _pt;
-
-public:
-    EmbindObject(int i, double d, std::string const& s) : _int(i), _double(d), _string(s), _pt(1.21, 6.123456789, "point") {}
-
-    int intValue() const { return _int; }
-    // void intValue(int value) { _int = value; }
-
-    double      doubleValue() const { return _double; }
-    std::string stringValue() const { return _string; }
-
-    void setDouble(double value) { _double = value; }
-
-    static void doIt() { std::cout << "EmbindObject" << std::endl; }
-};
-
-class EmbindManager
-{
-    std::vector<EmbindObject*> _data;
-
-    EmbindManager() {}
-
-public:
-    static EmbindManager& instance()
-    {
-        static EmbindManager opr;
-        return opr;
-    }
-
-    void add(EmbindObject* object) { _data.emplace_back(object); }
-
-    std::vector<EmbindObject*> data() const { return _data; }
-};
 #endif // !EMSCRIPTEN_BINDING_H
